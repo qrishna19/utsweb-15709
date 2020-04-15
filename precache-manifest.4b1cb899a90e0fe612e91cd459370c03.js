56b7e7ce9528fbf14c6267c2143fe5b38d020389
qrishna19 <qrishnaakbar@mail.ugm.ac.id>
2020-04-15 15:31:24 +0700

    Updates

---
+++ precache-manifest.4b1cb899a90e0fe612e91cd459370c03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a53222cbe0bfde1da81a1f7280ae77",
+    "url": "/utsweb-15709/index.html"
+  },
+  {
+    "revision": "fa3cd281e64d1e4b56c2",
+    "url": "/utsweb-15709/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "58f2de809c2918853885",
+    "url": "/utsweb-15709/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa3cd281e64d1e4b56c2",
+    "url": "/utsweb-15709/static/js/2.fe684f2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/utsweb-15709/static/js/2.fe684f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f2de809c2918853885",
+    "url": "/utsweb-15709/static/js/main.fea91a4f.chunk.js"
+  },
+  {
+    "revision": "5243a8339f8590ba7254",
+    "url": "/utsweb-15709/static/js/runtime-main.8a69a584.js"
+  }
+]);
